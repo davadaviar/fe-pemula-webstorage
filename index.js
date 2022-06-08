9c01bbfbe95bead6d9af9fa3a2008083d725ddf8
davadaviarsaputra <davadaviarsaputra2345@gmail.com>
2022-06-08 07:01:20 +0700

    Add destroy button dom

--- index.js
+++ index.js
@@ -125,3 +125,12 @@ window.addEventListener('beforeunload', function() {
     sessionStorage.setItem(sessionUserAttempsKey, 0);
     sessionUserAttempsField.innerText = this.sessionStorage.getItem(sessionUserAttempsKey);
 })
+
+destroyDataButton.addEventListener('click', function() {
+    sessionStorage.removeItem(sessionAnswerKey);
+    sessionStorage.removeItem(sessionUserAttempsKey);
+    sessionStorage.removeItem(sessionUserIsPlayingKey);
+    localStorage.removeItem(localTotalVictoryKey);
+    localStorage.removeItem(localMaximumAttempsKey);
+    alert('Mohon me-refresh halaman ini kembali!')
+})
